a3906fd5e3bc1c59a3aa790c90647ce4c8e6c5f1
jjfeng <jean.feng@gmail.com>
2020-07-24 16:40:30 -0700

    update cpmile

--- compile.sh
+++ compile.sh
@@ -1,3 +1,6 @@
+# First, go to paperpile and get bib files for the preprint and the published manuscripts
+# Then run these lines
+python bib_underliner.py
 pdflatex cv.tex
 bibtex cv.aux
 bibtex paper.aux
