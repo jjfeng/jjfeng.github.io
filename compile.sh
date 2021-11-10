3ec45c9a01c204f276c51322ee8bd9e336fe0a77
Jean Feng <jean.feng@gmail.com>
2021-11-10 14:43:22 -0800

    update website

--- compile.sh
+++ compile.sh
@@ -1,5 +1,6 @@
 # First, go to paperpile and get bib files for the preprint and the published manuscripts
 # Then run these lines
+. venv/bin/activate
 python3 bib_underliner.py
 python3 generate_publications.py
 pdflatex cv.tex
